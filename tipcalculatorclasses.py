d6b6a6e6617e8c59cf55e46d5437416f9c76bb82
Teresa M <teresawithoutah@gmail.com>
2021-04-15 10:46:25 -0400

    version 2 of tip calculator
    implemented classes for this version

---
+++ tipcalculatorclasses.py
@@ -0,0 +1,37 @@
+import math
+
+def calculation(total_bill,guests):  
+    tip = (15/100)*total_bill
+    #calculates final amount of bill with the tip included
+    bill_with_tip = tip+total_bill
+    
+    return bill_with_tip
+
+
+def split_bill(total_bill,guests):
+ 
+  
+    final_bill = calculation(total_bill,guests)
+    split_tip= final_bill/guests
+    
+    print("\nSplit bill ", split_tip)
+    
+    i = 1
+
+    #prints how much each guest will need to pay
+    while ( i <= guests):
+        print ('Guest',i,'will have to pay:', split_tip)
+        i = i + 1
+
+
+def main(): 
+    
+    total_bill = float(input("Enter total bill: "))
+    guests = float(input("Enter number of guests: "))
+
+    split_bill(total_bill,guests)
+        
+    
+if __name__ == '__main__':
+    main()
+ 
