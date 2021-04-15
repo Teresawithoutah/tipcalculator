7db6fba8e4953ae25a95fec778b79957e642d555
Teresa M <teresawithoutah@gmail.com>
2021-04-15 14:45:44 -0400

    two cases added
    determines if two values are dividable or not

--- test-short.py
+++ test-short.py
@@ -1,13 +1,15 @@
 import unittest 
-import short
+import tipcalculatorclasses
 
 class Test(unittest.TestCase):
 
  #   def test_total_bill(self): #it checks the main method
   #      self.assertRaises(ValueError, short.process_input("",1,1,3))
     
-    def test(self):
-        self.assertRaises(ValueError, short.process_input(102,2))
+    def test_dividable1(self):
+        self.assertTrue(tipcalculatorclasses.process_input(102,2))
+    def test_dividable2(self):
+        self.assertTrue(tipcalculatorclasses.process_input(10,3))
 
 
 
