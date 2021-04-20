6c2821c4ce6ffed8fa8581e2fe17bdd1e0e0a95c
Teresa M <teresawithoutah@gmail.com>
2021-04-19 22:34:45 -0400

    all testcases work
    TYPEERROR NOT VALUEERROR

--- test-short.py
+++ test-short.py
@@ -2,17 +2,17 @@ import unittest
 import tipcalculatorclasses
 
 class Test(unittest.TestCase):
-
- #   def test_total_bill(self): #it checks the main method
-  #      self.assertRaises(ValueError, short.process_input("",1,1,3))
+    def test_totalbill(self): #it checks the main method
+        self.assertRaises(TypeError, tipcalculatorclasses.process_input("f",2))
     
-    def test_dividable1(self):
+    def test_guests(self): #it checks the main method
+        self.assertRaises(TypeError, tipcalculatorclasses.process_input(2.0,"h"))
+   
+    def test_isdividable1(self): #it checks the main method
         self.assertTrue(tipcalculatorclasses.process_input(102,2))
-    def test_dividable2(self):
-        self.assertTrue(tipcalculatorclasses.process_input(10,3))
-
-
-
+    
+    def test_isdividable2(self): #it checks the main method
+        self.assertFalse(tipcalculatorclasses.process_input(10,3))
 
 if __name__ == '__main__':
     unittest.main()
