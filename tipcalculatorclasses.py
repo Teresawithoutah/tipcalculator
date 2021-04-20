5d74b8ff64a3fa6fce57632d461c96461ef5c781
Teresa M <teresawithoutah@gmail.com>
2021-04-19 21:54:53 -0400

    correct
    asks total bill, # of guests and prints out split bill + tax

--- tipcalculatorclasses.py
+++ tipcalculatorclasses.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 20:36:54 2021
+
+@author: teresawithoutah
+"""
+
 def calculation(total_bill,guests):  
-    tip = (15/100)*total_bill
+    tip = float(0.15/100)*float(total_bill)
     #calculates final amount of bill with the tip included
     bill_with_tip = tip+total_bill
     
-    return bill_with_tip
+    return bill_with_tip 
 
 
 def split_bill(total_bill,guests):
@@ -22,28 +30,45 @@ def split_bill(total_bill,guests):
         i = i + 1
 
 
-def main(): 
     
-    total_bill = float(input("Enter total bill: "))
-    guests = float(input("Enter number of guests: "))
+def process_input(total_bill,guests):      
+    try:
+        total_bill = float(total_bill)
+    except ValueError:
+        print ("Wrong entry for total bill")
+        return
+    
+    try:
+        guests = int(guests)
+    except ValueError:
+        print ("Wrong entry for guests")
+        return
 
-    split_bill(total_bill,guests)
-    process_input(total_bill,guests)
-        
+    split1 = split_bill(total_bill,guests)
+    print(split1)
     
-def process_input(total_bill,guests):
-    # if can divide without remainder
+        
+ # if can divide without remainder
     if total_bill % guests ==0: 
         print("dividable")
         return True
        
     else:
         print("not dividable")
-        return False    
+        return False   
+    
+def main(): 
+    
+    total_bill = input("Enter total bill: ")
+    guests = input("Enter number of guests: ")
+    
+    process_input(total_bill,guests)
+    
+
+
+
+  
         
-    
-    
-    
 if __name__ == '__main__':
     main()
  
